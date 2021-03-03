ec304234e4f6d2e8ad514622c493d01225a01e43
Andrey Stepanov <an.stepanov@skoltech.ru>
2021-03-03 16:54:32 +0300

    Pagination animation added

--- src/app/components/molecules/pagination/pagination.component.ts
+++ src/app/components/molecules/pagination/pagination.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pagination',
@@ -9,9 +10,14 @@ export class PaginationComponent implements OnInit, OnChanges {
   @Input() currentPage: number = null;
   @Input() totalPages: number = null;
 
-  constructor() {}
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.router.navigate(['/project/tasks/123/submits'], {
+      queryParams: { page: this.currentPage },
+      queryParamsHandling: 'merge',
+    });
+  }
 
   ngOnChanges(): void {
     if (this.currentPage && this.totalPages) {
@@ -21,5 +27,28 @@ export class PaginationComponent implements OnInit, OnChanges {
         this.totalPages
       );
     }
+  }
+
+  handlePageClick(i: number) {
+    if (i > 0 && i < this.totalPages) this.currentPage = i + 1;
+  }
+
+  handlePrevPageClick() {
+    this.currentPage = this.currentPage > 1 ? this.currentPage - 1 : 1;
+    this.router.navigate(['/project/tasks/123/submits'], {
+      queryParams: { page: this.currentPage },
+      queryParamsHandling: 'merge',
+    });
+  }
+
+  handleNextPageClick() {
+    this.currentPage =
+      this.currentPage < this.totalPages
+        ? this.currentPage + 1
+        : this.totalPages;
+    this.router.navigate(['/project/tasks/123/submits'], {
+      queryParams: { page: this.currentPage },
+      queryParamsHandling: 'merge',
+    });
   }
 }
--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,3 +1,3 @@
 export default {
-  API: '/dataskai2',
+  API: '',
 };
